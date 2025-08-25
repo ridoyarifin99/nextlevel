a5bcc05772041440faf17d13708892aff1be24dc
ridoyarifin99 <ridoyarifin99@gmail.com>
2025-08-25 13:35:51 +0600

    update

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  build: {
-    minify: 'esbuild', // 'esbuild' is the default for JS and CSS
-    cssMinify: 'lightningcss', // Use Lightning CSS for CSS minification
-  },
-});
