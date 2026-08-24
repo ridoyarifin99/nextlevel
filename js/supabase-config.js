fe0dea89b11efab2d027da9452d6a419682d2a9c
ridoyarifin99 <ridoyarifin99@gmail.com>
2026-08-25 04:25:31 +0600

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,7 +1,13 @@
-const SUPABASE_URL = "https://zrptkmjdltqdjzrpogyo.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_KcWSkkO1L4z0U6UUfZijyw_KIJ_d5m7";
+"use strict";
 
-const supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-);
+window.SUPABASE_URL =
+    "https://zrptkmjdltqdjzrpogyo.supabase.co";
+
+window.SUPABASE_ANON_KEY =
+    "sb_publishable_KcWSkkO1L4z0U6UUfZijyw_KIJ_d5m7";
+
+window.supabaseClient =
+    window.supabase.createClient(
+        window.SUPABASE_URL,
+        window.SUPABASE_ANON_KEY
+    );
