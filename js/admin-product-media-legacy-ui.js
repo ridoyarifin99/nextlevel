fe73834d62171b0450a2e1656009c5ae55247139
ridoyarifin99 <ridoyarifin99@gmail.com>
2026-09-10 03:10:24 +0600

    Remove legacy admin media UI shim

--- js/admin-product-media-legacy-ui.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-(() => {
-  function hideLegacyField() {
-    const input = document.getElementById("pImage");
-    const field = input?.closest(".field");
-    if (field) field.style.display = "none";
-  }
-  if (document.readyState === "loading") document.addEventListener("DOMContentLoaded", hideLegacyField, {once:true}); else hideLegacyField();
-})();
