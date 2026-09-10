38044eb43a33fb78a51b1d21c1ce87dddfd9737c
ridoyarifin99 <ridoyarifin99@gmail.com>
2026-09-10 17:06:25 +0600

    Inject promotion manager into central product manager

---
+++ api/admin-products-shell.js
@@ -0,0 +1,11 @@
+"use strict";
+const fs = require("fs");
+const path = require("path");
+module.exports = async function handler(req,res){
+  if(req.method!=="GET"&&req.method!=="HEAD"){res.statusCode=405;res.setHeader("Allow","GET, HEAD");return res.end("Method Not Allowed");}
+  try{
+    let html=fs.readFileSync(path.join(__dirname,"..","admin-products.html"),"utf8");
+    if(!/promo-management\.js/i.test(html)) html=html.replace(/<\/body>/i,'<script src="/js/promo-management.js"></script></body>');
+    res.statusCode=200;res.setHeader("Content-Type","text/html; charset=utf-8");res.setHeader("Cache-Control","no-store, max-age=0");return res.end(html);
+  }catch(e){res.statusCode=500;return res.end("Admin product manager unavailable");}
+};
